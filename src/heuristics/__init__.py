45c2046a0695bfac88e8d9f941d3dc856fabac48
Choonyong Chan <chanchoonchan@hotmail.sg>
2026-04-28 12:15:32 +0000

    fix: remove try-except from heuristics __init__.py to match spec

--- src/heuristics/__init__.py
+++ src/heuristics/__init__.py
@@ -1,8 +1,4 @@
 from src.heuristics.base_heuristic import BaseHeuristic
-
-try:
-    from src.heuristics.heuristic import Heuristic
-except ImportError:
-    Heuristic = None
+from src.heuristics.heuristic import Heuristic
 
 __all__ = ["BaseHeuristic", "Heuristic"]
